3e95b28040f32cb48a2ce03d3517b3ecef50da78
smdeox <smdeox@live.com>
2020-09-17 18:03:49 -0400

    Test db info added

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Set db file to users.db
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+# DB
+db = SQLAlchemy(app)
+class Users(db.Model):
+    id = db.Column('student_id', db.Integer, primary_key = True) # primary_key makes it so that this value is unique and can be used to identify this record.
+    username = db.Column(db.String(24))
+    email = db.Column(db.String(64))
+    pwd = db.Column(db.String(64))
+
+    # Constructor
+    def __init__(self, username, email, pwd):
+        self.username = username
+        self.email = email
+        self.pwd = pwd
 
 #First route
 @app.route('/')
